1254cc12fabd755d60f8f21aa64aecf7ef02c1c2
Eddie Obrams <eddieobrams@gmail.com>
2023-05-27 22:09:35 +0100

    made navbar into a js script, completed login and signup forms designs, made snake game in js.

---
+++ scripts/navbar.js
@@ -0,0 +1,12 @@
+const navigationBar = `
+    <div class="menu-icon"></div>
+    <div class="nav-item-div">
+        <a class=" nav-item" href="snake.html">Snake</a>
+    </div>
+    <h1 class="title">SmarboLab</h1>
+    <div class="nav-item-div">
+        <a class=" nav-item" href="app.html">App</a>
+    </div>
+`;
+
+document.getElementById('navBar').innerHTML = navigationBar;
