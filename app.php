4bffa462ff8659f5fb0e6160a33fb2d9e920746b
Eddie Obrams <eddieobrams@gmail.com>
2023-05-28 00:00:58 +0100

    added you are logged out box and made it actually work

--- app.php
+++ app.php
@@ -1,3 +1,5 @@
+<?php require_once 'controllers/authController.php'; ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -12,12 +14,14 @@
   <div class="container">
       <nav class="top-nav" id="navBar"></nav>
     <main>
+      <?php if(!isset($_SESSION['id'])): ?>
       <div class="logged-out-box main-child">
         <h1 id="child">You are logged out.</h1>
         <p id="child" class="message" style="text-align: center;">It seems you are currently not logged in to an account.<br>Please log in to access the rest of the site.</p>
         <a href="login.php" class="button" id="child">Log In</a>
         <a href="signup.php" class="button" id="child">Sign Up</a>
       </div>
+      <? endif; ?>
       
     </main>
   </div>
