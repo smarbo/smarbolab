37aa9fe18b85003436b65161e9369492b80d576d
Eddie Obrams <eddieobrams@gmail.com>
2023-05-28 23:32:05 +0100

    navbar.js: added icon to title

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,7 +3,7 @@ const navigationBar = `
     <div class="nav-item-div">
         <a class=" nav-item" href="snake.php">Snake</a>
     </div>
-    <h1 class="title">SmarboLab</h1>
+    <h1 class="title">SmarboLab<i class='fa-sharp fa-solid fa-flask-round-potion'></i></h1>
     <div class="nav-item-div">
         <a class=" nav-item" href="app.php">App</a>
     </div>
