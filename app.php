9bf1e51bffae48cdd4dcdf75928bec794df00e76
Eddie Obrams <eddieobrams@gmail.com>
2023-05-29 12:26:52 +0100

    mobile responsive?

--- app.php
+++ app.php
@@ -8,6 +8,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
   <link href="styles/main.css" rel="stylesheet">
   <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <link href="styles/main.css" rel="stylesheet">
   <link href="styles/button.css" rel="stylesheet">
   <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
